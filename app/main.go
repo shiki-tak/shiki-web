412ff0512c64f52c6bd57da88e9aae96ef45e8d1
shiki <shiki.tak@gmail.com>
2020-03-29 21:21:14 +0900

    add skaffold k8s

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Shiki-Web!")
+		return c.String(http.StatusOK, "Hello, Shiki-Web!!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
